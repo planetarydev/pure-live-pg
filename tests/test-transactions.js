51fa09c46c21e24e03a62a8fbed29e73dd18f57e
planetarydev <planetarydevelop@gmail.com>
2017-10-26 04:43:42 +0200

    endless loop on reactivity during transaction

--- tests/test-transactions.js
+++ tests/test-transactions.js
@@ -174,5 +174,101 @@ describe('PgDatabase', function() {
 			});
 		});
 
+		describe('Reactivity during transactions', function() {
+			it('should not wait for optimistic ui changes', function(done) {
+				let db = new PGDatabase({
+					enablePooling: true,
+					connect: {
+						user: DB_USER,
+						host: DB_HOST,
+						database: DB_DATABASE,
+						password: DB_PASSWORD,
+						port: DB_PORT,
+						max: DB_MAX_CONNECTION
+					}
+				});
+				var People;
+				var peopleCount = 0;
+
+				async.waterfall([
+					function(callback){
+						db.connect(callback);
+					},
+					function(callback){
+						People = db.Table('people', callback);
+					},
+					function(callback){
+						// remove all people named transaction to run the tests twice
+						People.remove({
+							last_name: 'Transaction'
+						}, (error, result) => {
+							if (error) return callback(error);
+							return callback();
+						});
+					},
+					function(callback){
+						var reactivePeoples = People.find({
+							last_name: 'Transaction'
+						}, {
+							clientId: 'TestClient'
+						});
+
+						reactivePeoples.on('added', (id, doc) => {
+							peopleCount++
+						});
+
+						reactivePeoples.run((error)=>{
+							if (error) return callback(error);
+							return callback();
+						});
+					},
+					function(callback){
+						db.beginTransaction((error, transactionHandle)=>{
+							if (error) return callback(error);
+							return callback(null, transactionHandle);
+						});
+					},
+					function(transactionHandle, callback){
+						People.insert({
+							_id: 'Mike001',
+							first_name: 'Mike',
+							last_name: 'Transaction'
+						}, { transaction: transactionHandle, clientId: 'TestClient' }, (error, result) => {
+							if (error) return callback(error);
+							return callback(null, transactionHandle)
+						});
+					},
+					function(transactionHandle, callback){
+						People.insert({
+							_id: 'Mike002',
+							first_name: 'Rene',
+							last_name: 'Transaction'
+						}, { transaction: transactionHandle, clientId: 'TestClient' }, (error, result) => {
+							if (error) return callback(error);
+							return callback(null, transactionHandle)
+						});
+					},
+					function(transactionHandle, callback){
+						transactionHandle.commit((error, result) => {
+							if (error) return callback(error);
+
+							People.select({
+								last_name: 'Transaction'
+							}, (error, result) => {
+								if (error) return callback(error);
+
+								expect(result.rowCount).to.equal(2);
+
+								return callback();
+							});
+						});
+					}
+				], (error)=>{
+					db.end();
+					return done();
+				});
+			});
+		});
+
 	});
 });
